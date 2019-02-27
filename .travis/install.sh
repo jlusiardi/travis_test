9c1b18140ba08d9dacdc20ad7ebd248f6ade1b9f
Joachim Lusiardi <joachim@lusiardi.de>
2019-02-27 07:19:32 +0100

    more work for OS X and Windows

---
+++ .travis/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    sudo apt-get update;
+    sudo apt-get install -y build-essential python3-dev libdbus-1-dev libdbus-glib-1-dev libgirepository1.0-dev;
+    pip install -r requirements_linux.txt
+fi
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    brew update
+    # see https://github.com/pyenv/pyenv/wiki#suggested-build-environment for Mac OS X
+    brew install openssl readline sqlite3 xz zlib
+    # pyenv is already installed on a test node
+    brew outdated pyenv || brew upgrade pyenv
+    pyenv install --list
+    pyenv install $PYTHON
+    pyenv shell $PYTHON
+    python --version
+    python3 --version
+    pip --version
+    pip3 --version
+    pip3 install -r requirements_osx.txt
+fi
+
+if [ "$TRAVIS_OS_NAME" == "windows" ]; then
+    choco install python3
+    python3 --version
+    pip3 --version
+fi
+
